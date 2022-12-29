62d2d1ea6f8470ed54901a071f4e110ec9c40bba
GeorgeDV123 <115605263+GeorgeDV123@users.noreply.github.com>
2022-12-29 11:45:16 +1100

    Navbar finished, SCSS started

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
+import { Navbar1 } from './componets/Navbar'
 import { About } from './Pages/About'
 import { Home } from './Pages/Home'
 import { Store } from './Pages/Store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './SCSS/style.scss'
 
 function App() {
 
   return (
+    <>
+    <Navbar1 />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/store' element={<Store />} />
       <Route path='/about' element={<About />} />
     </Routes>
+    </>
   )
 }
 
